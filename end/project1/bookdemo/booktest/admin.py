f3d4d385fbed0639a93ea1086156437d16a14dfb
tqy <1980739637@qq.com>
2020-02-13 11:36:41 +0800

    后台管理功能的丰富

--- end/project1/bookdemo/booktest/admin.py
+++ end/project1/bookdemo/booktest/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
+# 定义后端显示界面
 # Register your models here.
 # django自带的后天管理操作需要在此实现
 
 # 注册自己需要管理的模型
 
 from .models import Book,Hero
-admin.site.register(Book)
-admin.site.register(Hero)
+class HeroInline(admin.StackedInline):
+    """
+    定义关联类
+    """
+    model = Hero
+    extra = 5
+
+class BookAdmin(ModelAdmin):
+    """
+    定义模型管理类
+    通过该类可以修改后台页面
+    """
+    list_display = ('title', 'price', 'pub_data')
+    # list_per_page = 1
+    search_fields = ('title', 'price')
+    list_filter = ('title',)
+    inlines = [HeroInline]
+
+class HeroAdmin(ModelAdmin):
+    list_display = ('name', 'gender', 'content', 'book')
+    search_fields = ('gender',)
+    list_filter = ('gender',)
+
+admin.site.register (Book, BookAdmin)
+admin.site.register (Hero, HeroAdmin)
