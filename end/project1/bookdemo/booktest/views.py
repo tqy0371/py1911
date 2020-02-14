627772ba45cb692b1bcde45cb191ced8142a00dc
tqy <1980739637@qq.com>
2020-02-14 17:56:46 +0800

    增加英雄的添加、删除和修改功能

--- end/project1/bookdemo/booktest/views.py
+++ end/project1/bookdemo/booktest/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.template import loader
 from .models import Book,Hero
 
 # Create your views here.
 
-from django.http import HttpResponse
-def index(requests):
+from django.http import HttpResponse, HttpResponseRedirect
+def index(request):
     # return HttpResponse("这里是首页")
     # 获取模板
     # template = loader.get_template('index.html')
@@ -15,9 +15,9 @@ def index(requests):
     # result = template.render(context)
     # 将渲染结果使用httpresponse返回
     # return HttpResponse(result)
-    return render(requests,'index.html',{'books':books})
+    return render(request, 'index.html', {'books': books})
 
-def about(requsets):
+def about(requset):
     return HttpResponse("这里是关于页面")
 
 def detail(request,bookid):
@@ -26,7 +26,45 @@ def detail(request,bookid):
     # context = {"book":book}
     # result = template.render(context)
     # return HttpResponse(result)
-    return render(request,'detail.html',{'book':book})
+    return render(request, 'detail.html', {'book': book})
+
+def deletebook(request, bookid):
+    book =Book.objects.get(id=bookid)
+    book.delete()
+    #删除一本书之后仍然回到列表页
+    # return HttpResponseRedirect(redirect_to='/')
+    return redirect(to='/')
+def deletehero(request, heroid):
+    hero = Hero.objects.get(id=heroid)
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse("booktest:detail", args=(bookid,))
+    return redirect(to=url)
+
+def addhero(request,bookid):
+    if request.method == "GET":
+        return render(request, 'addhero.html')
+    elif request.method == "POST":
+        hero = Hero()
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse("booktest:detail", args=(bookid,))
+        return redirect(to=url)
+
+def edithero(request,heroid):
+    hero = Hero.objects.get(id=heroid)
+    if request.method == 'GET':
+        return render(request, 'edithero.html', {"hero":hero})
+    elif request.method == 'POST':
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.save()
+        url = reverse("booktest:detail", args=(hero.book.id,))
+        return redirect(to=url)
 
 # 使用Django模板
 
